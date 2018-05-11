dcd1d8fc0e45f53c8918ff46a922eb855cc4c0fc
Musu1 <muskan.agarwal899@gmail.com>
2018-05-11 14:05:13 +0530

    Testing the flaws of code and also rectifying overflow by type casting

---
+++ multiply.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+ int main(){
+float n,d;
+float c;
+ printf("\nEnter the first number\n");
+ scanf("%f",&n);
+ printf("enter the second number\n");
+ scanf("%f",&d);
+ c=(float)(d*n);
+ printf("The answer is%d",c);
+ }
