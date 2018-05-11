0d8e46004fddb78126b9cc4430b9e9a5b76e0733
Musu1 <muskan.agarwal899@gmail.com>
2018-05-11 14:10:24 +0530

    Testing the flaws of code and also rectifying overflow by type casting

---
+++ type_cast.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+ int main(){
+ int n,c,d;
+ printf("\nEnter the first number\n");
+ scanf("%d",&n);
+ n=(float)n;
+ printf("enter the second number\n");
+ scanf("%d",&d);
+ c=(float)d+n;
+ printf("The answer is%d",c);
+ }
