fa2357084270daef00db2f394b94ab6985775bfc
Musu1 <muskan.agarwal899@gmail.com>
2018-05-11 14:11:26 +0530

    using type casting

--- prime_number_code_testing.c
+++ prime_number_code_testing.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+int main(){
 
+int n,i,c=0;
+printf("enter the number");
+scanf("%d",&n);
+for(i=0;i<=n;i++){
+    if(n%i==0){
+        c++;
+    }
+}
+if(c==2){
+    printf("given number is prime");
+}
+else{
+    printf("This is not a prime number");
+}
+return 0;
+}
