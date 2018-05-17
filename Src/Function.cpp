1e8e73ba1d4f2d53c7add8e14d3b2142b32a18a7
Yogesh Modi <16uec134@lnmiit.ac.in>
2018-05-17 05:53:36 +0000

    Added 8 functions to be used in main file

--- Src/Function.cpp
+++ Src/Function.cpp
@@ -1,7 +1,59 @@
-#include <iostream>
+int add(int a,int b){
+    ans=a+b;
+    return ans;
+}
 
-int main()
-{
-    std::cout << "Hello, world!\n";
-    return 0;
+int sub(int a,int b){
+    ans=a-b;
+    return ans;
+}
+
+int mul(int a,int b){
+    ans=a*b;
+    return ans;
+}
+
+int divide(int a,int b){
+    ans=a/b;
+    return ans;
+}
+ 
+void initStack(){
+    TOP=-1;
+}
+
+int isEmpty(){
+    if(TOP==-1)
+        return 1;
+    else
+        return 0;
+}
+ 
+int isFull(){
+    if(TOP==MAX-1)
+        return 1;
+    else   
+        return 0;
+}
+ 
+void push(int num){
+    if(isFull()){
+        cout<<"STACK is FULL."<<endl;
+        return; 
+    }
+    ++TOP;
+    STACK[TOP]=num;
+}
+ 
+void display(){
+    int i;
+    cout<<"Your memory is:";
+    if(isEmpty()){
+        cout<<"Nothing in memory"<<endl;
+        return;
+    }
+    for(i=TOP;i>=0;i--){
+        cout<<STACK[i]<<" ";
+    }
+    cout<<endl;
 }
