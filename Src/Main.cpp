0abf84d576c4e604221e5cda3e12f0f37a30e42b
Yogesh Modi <16uec134@lnmiit.ac.in>
2018-05-17 07:46:25 +0000

    Corrected the code by making some changes

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <stdlib.h>
+#include "Function.cpp"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello, world!\n";
+    initStack();
+    int firstNumber, secondNumber,output;
+    char operation;
+    cout << "Instructions:\n\nPress + to add\nPress - to subtract\nPress * to multiply\nPress / to divide\nPress c to clear\nPress m to save\n " ;
+    cin >> firstNumber >> operation >>secondNumber;
+    output=choose(firstNumber,secondNumber,operation);
+    cout << output;
+    for(;;){
+        char continuosOperation;
+        int UserSubsequentInput;
+        cin >> continuosOperation;
+        if(continuosOperation == 'c'){
+        display();
+        exit(0);
+         }
+        else if(continuosOperation == 'm'){
+            push(output);
+            continue;
+        }
+        cin >> UserSubsequentInput;
+        output=choose(output,UserSubsequentInput,continuosOperation);
+        cout<<output;
+    }
+    
     return 0;
 }
