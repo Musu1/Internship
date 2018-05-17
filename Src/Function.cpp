c728dfba514d39549598cba303f06e005829393d
Yogesh Modi <16uec134@lnmiit.ac.in>
2018-05-17 06:22:55 +0000

    Added the documentation in the code

--- Src/Function.cpp
+++ Src/Function.cpp
@@ -1,26 +1,78 @@
+/*Function: add
+    Add two integers
+  Parameters : 
+    a - The first integer
+    b - The second integer
+  Return :
+    Gives the addition of two numbers
+*/ 
+
+int functionResult;
 int add(int a,int b){
-    ans=a+b;
-    return ans;
+    functionResult=a+b;
+    return functionResult;
 }
+
+/*Function: sub
+    Subtracts two integers
+  Parameters : 
+    a - The first integer
+    b - The second integer
+  Return :
+    Gives the substraction of two numbers
+*/
 
 int sub(int a,int b){
-    ans=a-b;
-    return ans;
+    functionResult=a-b;
+    return functionResult;
 }
+
+/*Function: mul
+    Multiplies two integers
+  Parameters : 
+    a - The first integer
+    b - The second integer
+  Return :
+    Gives the product of two numbers
+*/
 
 int mul(int a,int b){
-    ans=a*b;
-    return ans;
+    functionResult=a*b;
+    return functionResult;
 }
 
+/*Function: div
+    Divide two integers
+  Parameters : 
+    a - The first integer
+    b - The second integer
+  Return :
+    Gives the division of two numbers
+*/
+
 int divide(int a,int b){
-    ans=a/b;
-    return ans;
+    float functionResult;
+    functionResult=a/b;
+    return functionResult;
 }
+
+/*Function: initStack
+    Initializes a stack by initializing Top pointer as -1
+  Parameters : 
+    None required
+*/
  
 void initStack(){
     TOP=-1;
 }
+
+/*Function: isEmpty
+    Checks wether the stack is empty or not
+  Parameters : 
+    None
+  Return :
+    Gives 1 if empty stack else 0
+*/
 
 int isEmpty(){
     if(TOP==-1)
@@ -29,12 +81,25 @@ int isEmpty(){
         return 0;
 }
  
+/*Function: isFull
+    Check wether the stack is full or not
+  Parameters : 
+    None
+  Return :
+    Gives 1 if stackk is full or 0 if stack is empty
+*/ 
 int isFull(){
     if(TOP==MAX-1)
         return 1;
     else   
         return 0;
 }
+
+/*Function: push
+    Adds a new integer in stack
+  Parameters : 
+    num - The number which you want to add in the stack.
+*/
  
 void push(int num){
     if(isFull()){
@@ -45,6 +110,14 @@ void push(int num){
     STACK[TOP]=num;
 }
  
+ /*Function: display
+    Add two integers
+  Parameters : 
+    None
+  Return :
+    Prints the stack.
+*/
+
 void display(){
     int i;
     cout<<"Your memory is:";
