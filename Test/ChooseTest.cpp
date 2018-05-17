40683373c6132a7966a6e404ffb32f2d17a6d7be
Yogesh Modi <16uec134@lnmiit.ac.in>
2018-05-17 11:31:54 +0000

    Testing the choose functionality of function.cpp

---
+++ Test/ChooseTest.cpp
@@ -0,0 +1,18 @@
+class Mockfunctions L public functions{
+public:
+    MOCK_METHOD2(add, int(int firstNumber,int secondNumber));
+    MOCK_METHOD2(sub, int(int firstNumber,int secondNumber));
+    MOCK_METHOD2(mul, int(int firstNumber,int secondNumber));
+    MOCK_METHOD2(divide, int(int firstNumber,int secondNumber));
+}
+
+TEST(Choose,add){
+    Mockfunctions mf;
+    int c=8+3;
+    EXPECT_CALL(mf, add(8,3)).
+    .Times(1)
+    .willOnce(Return c);
+    
+    int retvalue=choose(8,3,'+');
+    EXPECT_EQ(retvalue,c);
+}
