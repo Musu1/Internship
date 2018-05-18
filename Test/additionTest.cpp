a0343e4db1aece7aaabd917148c120c4fed940af
Yogesh Modi <16uec134@lnmiit.ac.in>
2018-05-18 02:30:05 +0000

    Added object feature

--- Test/additionTest.cpp
+++ Test/additionTest.cpp
@@ -1,31 +1,31 @@
 //Test to add two positive numbers
 TEST(AddTwoNumber, PositiveNos) {
     cout<<"Testing the addition of two positive Numbers"<<endl;
-    EXPECT_EQ(5,add(2,3));
+    EXPECT_EQ(5,key.add(2,3));
  }
 //Test to add two negative numbers 
 TEST(AddTwoNumber, NegativeNos) {
     cout<<"Testing the addition of two Negative Numbers"<<endl;
-    EXPECT_EQ(-5,add(-2,-3));
+    EXPECT_EQ(-5,key.add(-2,-3));
  }
 //Test to add two numbers of opposite sign 
  TEST(AddTwoNumber, OppositeSign) {
     cout<<"Testing the addition of two Number with oppsite sign"<<endl;
-    EXPECT_EQ(-1,add(2,-3));
+    EXPECT_EQ(-1,key.add(2,-3));
  }
 //Test to add two numbers out of int data type limit 
  TEST(AddTwoNumber, NotInLimit) {
     cout<<"Testing the overflow in addition"<<endl;
-    EXPECT_EQ(6000000000000,add(3000000000000,3000000000000));
+    EXPECT_EQ(6000000000000,key.add(3000000000000,3000000000000));
  }
  //Test to add zero to any number
 TEST(AddTwoNumber, ZeroAddition) {
      cout<<"Testing the addition of zer and a number"<<endl;
-     EXPECT_EQ(3,add(0,3));
+     EXPECT_EQ(3,key.add(0,3));
  }
  //Test to add two chars
  TEST(AddTwoNumber,charAddition) {
      char a='a';
      cout<<"Testing the addition of two characters"<<endl;
-     EXPECT_EQ(0,add(8,a));
+     EXPECT_EQ(0,keyadd(8,a));
  }
--- Test/subtsractionTest.cpp
+++ Test/subtsractionTest.cpp
@@ -1,37 +1,37 @@
 //Testing the substraction of two positive integer
 TEST(SubTwoNumber, PositiveNos) {
     cout<<"Testing the substraction of two positive Numbers"<<endl;
-    EXPECT_EQ(5,sub(8,3));
+    EXPECT_EQ(5,key.sub(8,3));
  }
 //Testing the substraction of two negative number
 TEST(SubTwoNumber, NegativeNos) {
     cout<<"Testing the substraction of two Negative Numbers"<<endl;
-    EXPECT_EQ(-4,sub(-7,-3));
+    EXPECT_EQ(-4,key.sub(-7,-3));
  }
 //Testing the substraction of two number with opposite sign 
  TEST(SubTwoNumber, OppositeSignCase1) {
     cout<<"Testing the substraction of two Number with oppsite sign"<<endl;
-    EXPECT_EQ(5,sub(2,-3));
+    EXPECT_EQ(5,key.sub(2,-3));
  }
 //Testing the substraction of tw numbers with opposite sign 2 
   TEST(SubTwoNumber, OppositeSignCase2) {
     cout<<"Testing the substraction of two Number with oppsite sign"<<endl;
-    EXPECT_EQ(-5,sub(-2,3));
+    EXPECT_EQ(-5,key.sub(-2,3));
  }
  
  //Testing the overflow in substraction
  TEST(SubTwoNumber, NotInLimit) {
     cout<<"Testing the overflow in substraction"<<endl;
-    EXPECT_EQ(7000000000000,sub(10000000000000,3000000000000));
+    EXPECT_EQ(7000000000000,key.sub(10000000000000,3000000000000));
  }
 //Testing the  
 TEST(SubTwoNumber, ZeroSubtraction){
     cout<<"Testing the substraction of zer and a number"<<endl;
-     EXPECT_EQ(-3,sub(0,3));
+     EXPECT_EQ(-3,key.sub(0,3));
  }
  
  TEST(SubTwoNumber,charSubractin) {
      char a='a';
      cout<<"Testing the substraction of two characters"<<endl;
-     EXPECT_EQ(0,add(8,a));
+     EXPECT_EQ(0,key.add(8,a));
  }
