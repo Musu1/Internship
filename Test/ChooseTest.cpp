8929d99b0b7268bd4ecb3dbc095110a6d2876578
Yogesh Modi <16uec134@lnmiit.ac.in>
2018-05-18 03:50:59 +0000

    code segmentation problem

--- Test/ChooseTest.cpp
+++ Test/ChooseTest.cpp
@@ -16,3 +16,15 @@ TEST(Choose,add){
     int retvalue=choose(8,3,'+');
     EXPECT_EQ(retvalue,c);
 }
+
+TEST(Choose,sub){
+    Mockfunctions mf;
+    int c=83;
+    EXPECT_CALL(mf, sub(8,3))
+    .Times(1)
+    .WillOnce(Return(c));
+    
+    int retvalue=choose(8,3,'-');
+    EXPECT_EQ(retvalue,c);
+    std::cout<<"hi"<<std::endl;
+}
